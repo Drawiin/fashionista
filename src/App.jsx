4aa7b988104f5d90f8ba196f869d562c7f95742e
Vinicius Santos <vini65599@gmail.com>
2020-04-21 19:01:15 -0400

    Abstraction Buttom Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header'
 
 function App() {
   return (
-    <Header title="Fashionista" />
+    <Header title="Fashionista" counter={0}/>
   )
 }
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
-
-import BagButton from '../BagButton'
+import { FiShoppingBag, FiSearch } from 'react-icons/fi'
  
 import './styles.css'
 
+import Button from '../Button'
 
-export default function header({title}){
+
+export default function header({title, counter}){
     return(
         <header className="header" >
             <h1 className="header__title">{title}</h1>
-            <BagButton amount={2}/>
+            <div>
+                <Button>
+                    <FiSearch size={28}/>
+                </Button>
+                
+                <Button counter={counter} >
+                    <FiShoppingBag size={28}/>
+                </Button>
+
+            </div>
         </header>
     )
 }

2309210c11e4a707213028102a1ac2ec99a30ce9
vinicius <vini65599@gmail.com>
2020-06-02 23:31:20 -0400

    create actions

--- src/ducks/actions/index.js
+++ src/ducks/actions/index.js
@@ -1,5 +1,17 @@
-import { ADD_PRODUCT } from '../constants/action-types';
+import {
+  ADD_PRODUCT,
+  REMOVE_PRODUCT,
+  DECREMENT_PRODUCT,
+} from '../constants/action-types';
 
 export function addProduct(payload) {
   return { type: ADD_PRODUCT, payload };
+}
+
+export function removeProduct(payload) {
+  return { type: REMOVE_PRODUCT, payload };
+}
+
+export function decrementProduct(payload) {
+  return { type: DECREMENT_PRODUCT, payload };
 }

ee079a4714f29c8132b3cafebc87d754692e075d
vinicius <vini65599@gmail.com>
2020-06-02 23:27:50 -0400

    Create remove itens reducers

--- src/ducks/reducers/index.js
+++ src/ducks/reducers/index.js
@@ -1,42 +1,71 @@
-import { ADD_PRODUCT, REMOVE_PRODUCT } from '../constants/action-types';
+import {
+  ADD_PRODUCT,
+  REMOVE_PRODUCT,
+  DECREMENT_PRODUCT,
+} from '../constants/action-types';
 
 const initialState = {
   shoppingCart: {},
 };
 
+function addProduct(state, payload) {
+  const newShoppingCart = { ...state.shoppingCart };
+
+  if (newShoppingCart[payload.sku]) {
+    newShoppingCart[payload.sku] = {
+      ...newShoppingCart[payload.sku],
+      ...{ quantity: newShoppingCart[payload.sku].quantity + 1 },
+    };
+  } else {
+    newShoppingCart[payload.sku] = {
+      quantity: 1,
+      name: payload.name,
+      image: payload.image,
+      price: payload.price,
+      installments: payload.installments,
+    };
+  }
+
+  return { ...state, ...{ shoppingCart: newShoppingCart } };
+}
+
+function removeProduct(state, payload) {
+  const newShoppingCart = { ...state.shoppingCart };
+  if (newShoppingCart[payload.sku]) {
+    delete newShoppingCart[payload.sku];
+  }
+  return { ...state, ...{ shoppingCart: newShoppingCart } };
+}
+
+function decrementProduct(state, payload) {
+  const newShoppingCart = { ...state.shoppingCart };
+
+  if (newShoppingCart[payload.sku]) {
+    if (newShoppingCart[payload.sku].quantity > 1) {
+      newShoppingCart[payload.sku] = {
+        ...newShoppingCart[payload.sku],
+        ...{ quantity: newShoppingCart[payload.sku] - 1 },
+      };
+    } else {
+      return removeProduct(state, payload);
+    }
+  }
+  return { ...state, ...{ shoppingCart: newShoppingCart } };
+}
+
 function rootReducer(state = initialState, action) {
   const { type, payload } = action;
 
   if (type === ADD_PRODUCT) {
-    const newShoppingCart = { ...state.shoppingCart };
-
-    if (newShoppingCart[payload.sku]) {
-      newShoppingCart[payload.sku] = {
-        quantity: newShoppingCart[payload.sku].quantity + 1,
-        name: payload.name,
-        image: payload.image,
-        price: payload.price,
-        installments: payload.installments,
-      };
-    } else {
-      newShoppingCart[payload.sku] = {
-        quantity: 1,
-        name: payload.name,
-        image: payload.image,
-        price: payload.price,
-        installments: payload.installments,
-      };
-    }
-
-    return { ...state, ...{ shoppingCart: newShoppingCart } };
+    return addProduct(state, payload);
   }
 
   if (type === REMOVE_PRODUCT) {
-    const newShoppingCart = { ...state.shoppingCart };
-    if (newShoppingCart[payload.sku]) {
-      delete newShoppingCart[payload.sku];
-    }
-    return { ...state, ...{ shoppingCart: newShoppingCart } };
+    return removeProduct(state, payload);
+  }
+
+  if (type === DECREMENT_PRODUCT) {
+    return decrementProduct(state, payload);
   }
 
   return state;
